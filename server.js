9d7ec3ac44f927b59aaf3be0255f347e85eeac31
Sithembiso Maphanga <maphangasthembiso@gmail.com>
2022-07-10 05:44:01 +0200

    server running start working with body parser

--- server.js
+++ server.js
@@ -2,15 +2,23 @@
 
 // require access to the express module
 const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
 
 // Creates an Express application. The express() function is a top-level function exported by the express module.
 const app = express();
+
+dotenv.config({path: 'config.env'})
+const PORT = process.env.PORT || 8080
+
+// LOG REQUESTS
+app.use(morgan('tiny'));
 
 app.get('/',(req,res) => {
     res.send("Crud Application");
 })
 
 
-app.listen(3000,()=>{
-    console.log(`Server is running obn http://localhost:${3000}`)
+app.listen(PORT,()=>{
+    console.log(`Server is running obn http://localhost:${PORT}`)
 })
